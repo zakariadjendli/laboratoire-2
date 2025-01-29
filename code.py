eb928b8480705b88bf940c4ea6622c1951e94af8
zakariadjendli <zdjendli@live.fr>
2025-01-29 13:30:51 -0500

    labo2
    lab2

---
+++ code.py
@@ -0,0 +1,8 @@
+x = 10
+print(type(x))
+y = 3.14
+print(type(y))
+z = "bonjour"
+print(type(z))
+a = True
+print(type(a))
---
+++ code2.py
@@ -0,0 +1,4 @@
+integer_number = 1200
+float_number = 1.200
+print("Value:",new_number)
+print("Data Type:",type(new_number))
